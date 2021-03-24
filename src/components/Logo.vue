11aa3acf78f72a8b744341f367c0c81fb8a35fb7
Wei Wu <535772489@qq.com>
2021-03-24 09:53:53 +0800

    初始版本

---
+++ src/components/Logo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <span class="logo logo-blue">军</span>
+        <span class="logo logo-red">事</span>
+        <span class="logo logo-yellow">问</span>
+        <span class="logo logo-blue">答</span>
+    </div>
+</template>
+<style lang="less" scoped>
+    .logo {
+        font-size: 6em;
+        font-weight: bolder;
+    }
+
+    .logo-blue {
+        color: #4285f4;
+    }
+
+    .logo-red {
+        color: #e84235;
+    }
+
+    .logo-yellow {
+        color: #fabb06;
+    }
+
+    .logo-green {
+        color: #34a853;
+    }
+
+    .logo-20d {
+        transform-origin: center;
+        transform: rotateZ(-20deg);
+        display: inline-block;
+    }
+</style>
+
